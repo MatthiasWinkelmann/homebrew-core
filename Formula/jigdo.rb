45821d5c7512b9ed4adabbc88128fc192f57316d
Bo Anderson <mail@boanderson.me>
2020-04-12 23:57:03 +0100

    jigdo 0.8.0

--- Formula/jigdo.rb
+++ Formula/jigdo.rb
@@ -1,12 +1,9 @@
-# Jigdo is dead upstream. It consists of two components: Jigdo, a GTK+ using GUI,
-# which is LONG dead and completely unfunctional, and jigdo-lite, a command-line
-# tool that has been on life support and still works. Only build the CLI tool.
 class Jigdo < Formula
   desc "Tool to distribute very large files over the internet"
-  homepage "http://atterer.org/jigdo/"
-  url "http://atterer.org/sites/atterer/files/2009-08/jigdo/jigdo-0.7.3.tar.bz2"
-  sha256 "875c069abad67ce67d032a9479228acdb37c8162236c0e768369505f264827f0"
-  revision 6
+  homepage "https://www.einval.com/~steve/software/jigdo/"
+  url "https://www.einval.com/~steve/software/jigdo/download/jigdo-0.8.0.tar.xz"
+  sha256 "f253f72b5719716a7f039877a97ebaf4ba96e877510fca0fb42010d0793db6a4"
+  head "https://git.einval.com/git/jigdo.git", :branch => "upstream"
 
   bottle do
     sha256 "bcde67883304312dcb904e44b17928a16ec9cb1c8a469e37b2832104178eb7b1" => :catalina
@@ -19,16 +16,12 @@ class Jigdo < Formula
   depends_on "berkeley-db"
   depends_on "wget"
 
-  # Use MacPorts patch for compilation on 10.9; this software is no longer developed.
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/e101570/jigdo/patch-src-compat.hh.diff"
-    sha256 "a21aa8bcc5a03a6daf47e0ab4e04f16e611e787a7ada7a6a87c8def738585646"
-  end
-
   def install
+    # truncate64 is Linux-specific
+    ENV.append_to_cflags "-Dtruncate64=truncate"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--disable-x11",
+                          "--with-gui=no",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
     system "make"
